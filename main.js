5adcff67f3054e80b212116c9ae2126fb94c3d20
Vinesh Kannan <vkannan3@hawk.iit.edu>
2016-10-15 17:34:17 -0500

    Passwords suck

--- main.js
+++ main.js
@@ -1,42 +1,105 @@
-"use strict";
-console.log("Loading browser sdk");
-
-var client = matrixcs.createClient("http://matrix.org");
-client.publicRooms(function (err, data) {
-    if (err) {
-	   console.error("err %s", JSON.stringify(err));
-       return;
+// Create VoxImplant instance
+var voxAPI = VoxImplant.getInstance();
+// Add event listeners
+voxAPI.addEventListener(VoxImplant.Events.SDKReady, onSdkReady);
+voxAPI.addEventListener(VoxImplant.Events.ConnectionEstablished, onConnectionEstablished);
+voxAPI.addEventListener(VoxImplant.Events.ConnectionFailed, onConnectionFailed);
+voxAPI.addEventListener(VoxImplant.Events.ConnectionClosed, onConnectionClosed);
+voxAPI.addEventListener(VoxImplant.Events.AuthResult, onAuthResult);
+voxAPI.addEventListener(VoxImplant.Events.IncomingCall, onIncomingCall);
+voxAPI.addEventListener(VoxImplant.Events.MicAccessResult, onMicAccessResult);
+ 
+// Initialize the SDK
+try {
+    voxAPI.init({ 
+        useRTCOnly: true, // force usage of WebRTC
+        micRequired: true, // ask mic/cam access before connection to VoxImplant
+        videoSupport: true  // enable video support
+    });
+} catch(e) {
+    // showing the message if browser doesn't support WebRTC
+    if (e.message == "NO_WEBRTC_SUPPORT") alert("WebRTC support isn't available");
+}
+ 
+// Now we can use SDK functions - establish connection with VoxImplant
+function onSdkReady(){        
+    voxAPI.connect(); // mic/cam access dialog will be shown after the function call
+}
+ 
+// Process mic/cam dialog input result
+function onMicAccessResult(e) {
+    if (e.result) {
+        // access was allowed
+    } else {
+        // access was denined - no connection will happen
     }
-    console.log(data);
-    console.log("Congratulations! The SDK is working on the browser!");
-    var result = document.getElementById("result");
-    result.innerHTML = "<p>The SDK appears to be working correctly.</p>";
-});
-
-var username = '@vkannan:matrix.org';
-var password = 'passw0rd';
-
-$.ajax({
-	url: 'https://matrix.org/_matrix/client/r0/login',
-	beforeSend: function(xhr){
-		//xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password));
-		console.log('send');
-	},
-	type: 'GET',
-	dataType: 'json',
-	contentType: 'application/json',
-	processData: false,
-	data: JSON.stringify({
-		flows: {
-			type: "m.login.password"
-		}
-	}),
-	success: function(data){
-		console.log('success');
-		console.log(data);
-	},
-	error: function(err){
-		console.log('error');
-		console.error(err);
-	}
-});
+}
+ 
+// Establishing connection with VoxImplant
+function onConnectionEstablished() {
+    // Authorization - show the dialog for user to let enter username/password and use login function
+    // Change application_user, application_name, account_name and application_user_password to your data for testing
+    var application_user = window.USER_CREDENTIALS.username;
+    var application_name = 'tadhack';
+    var account_name = 'vinesh';
+    var application_user_password = window.USER_CREDENTIALS.password;
+    voxAPI.login(application_user+"@"+application_name+"."+account_name+".voximplant.com", application_user_password);
+}
+ 
+// Couldn't establish connection with VoxImplant
+function onConnectionFailed() {
+    // Websockets or UDP connection is unavailable
+}
+ 
+// Connection with VoxImplant was closed
+function onConnectionClosed() {
+    // Can call connect here to reconnect
+}
+ 
+function onAuthResult(e) {
+    if (e.result) { 
+        // authorization was successful - can make/receive calls now
+    } else {
+        // authorization failed - check e.code to see the error code
+    }
+}
+ 
+var currentCall = null; // current call
+ 
+// handle incoming call
+function onIncomingCall(e) {
+    currentCall = e.call;
+    // add event listeners
+    currentCall.addEventListener(VoxImplant.CallEvents.Connected, onCallConnected);
+    currentCall.addEventListener(VoxImplant.CallEvents.Disconnected, onCallDisconnected);
+    currentCall.addEventListener(VoxImplant.CallEvents.Failed, onCallFailed);
+    // Answer automatically. It's better to show the dialog to let answer/reject the call in real app.
+    currentCall.answer();
+}
+ 
+// Make outbound call
+function createCall() {
+    // application_username - app username that will be dialed (with video)
+    currentCall = voxAPI.call(application_username, true, null, {"X-DirectCall": "true"});
+    // add event listeners
+    currentCall.addEventListener(VoxImplant.CallEvents.Connected, onCallConnected);
+    currentCall.addEventListener(VoxImplant.CallEvents.Disconnected, onCallDisconnected);
+    currentCall.addEventListener(VoxImplant.CallEvents.Failed, onCallFailed);
+}
+ 
+// Call connected
+function onCallConnected(e) {  
+  // Start sending video and show incoming video    
+  voxAPI.sendVideo(true);
+  currentCall.showRemoteVideo(true);
+}
+ 
+// Call disconnected
+function onCallDisconnected(e) {
+  currentCall = null;
+}
+ 
+// Call failed
+function onCallFailed(e) {
+  // Error code -  e.code, error reason - e.reason
+}
